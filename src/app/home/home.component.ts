e78d1420fdbf213fa08bc49a3d2fc12fef2db4ec
arguiS <skreamzu@pm.me>
2024-10-15 15:32:37 +0200

    update

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,17 @@
+import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  test() {
+    this.router.navigate(['/ratings']);
+  }
+}
