8ef7c7e25dfcf54d796249dc168ccda5610c7a29
argui <elias.mertin@ead-systeme.de>
2024-10-16 13:34:42 +0200

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin'); // Import the plugin
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: {
         index: './src/index.js',
-        nav: './src/nav.js'
+        nav: './src/nav.js',
+        db: './src/db.js'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/, 
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    'css-loader' 
+                ],
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
@@ -23,11 +36,19 @@ module.exports = {
             filename: 'nav.html',
             chunks: ['nav'],
         }),
+        new HtmlWebpackPlugin({
+            template: './src/db.html',
+            filename: 'db.html',
+            chunks: ['db'],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
         new CopyWebpackPlugin({
             patterns: [
-                { from: path.resolve(__dirname, 'src/songs.db'), to: path.resolve(__dirname, 'dist/songs.db') } // Copy the database
+                { from: path.resolve(__dirname, 'src/songs.db'), to: path.resolve(__dirname, 'dist/songs.db') } 
             ]
         }),
     ],
-    mode: 'production', // Set to 'development' if you're still developing
+    mode: 'production',
 };
